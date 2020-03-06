09757371d6fac9f8061249039de8e5ab0a768554
bhaktitud <myforework@live.com>
2020-03-06 08:18:21 +0700

    authentication added

--- router/login.js
+++ router/login.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Controller = require('../controller/ControllerUser')
+const authentication = require('../middleware/authentication')
 
 router.post('/register',Controller.register)
 router.post('/login',Controller.login)
